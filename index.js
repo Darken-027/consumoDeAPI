687de5e11abc6c3eaeaa93f007ad92c63ab2b6cd
Darken-027 <torut1638@gmail.com>
2024-08-30 10:00:41 -0500

    consumo de apis he imagen, nombre, status

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ function getCharacter(done){
 
     const result = fetch("https://rickandmortyapi.com/api/character");
     result
-    .then(Response => response.json())
+    .then(response => response.json())
     .then(data => {
         done(data)
     });
 
 }
 getCharacter(data => {
-    console.log(data);
+    data.results.forEach(personaje => {
+
+        const article = document.createRange().createContextualFragment(/* html */`
+            <article>
+            <div class="imagen-container">
+                <img src="${personaje.image}" alt="culebrarick¡¡">
+            </div>
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+        </article>
+            `);
+
+            const main = document.querySelector("main");
+            main.append();
+
+    });
 });
